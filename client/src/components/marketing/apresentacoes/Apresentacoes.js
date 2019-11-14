e59277bdb5957f44f5940e61979cbb6300cfb87c
Anakin Ribeiro <eduardor.contato@outlook.com>
2019-11-14 16:05:53 -0300

    14/11

--- client/src/components/marketing/apresentacoes/Apresentacoes.js
+++ client/src/components/marketing/apresentacoes/Apresentacoes.js
@@ -1,11 +1,11 @@
-import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
-import PropTypes from "prop-types";
-import { connect } from "react-redux";
-import { getApresentacoes } from "../../../actions/apresentacao";
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getApresentacoes } from '../../../actions/apresentacao';
 
-import Navbar from "../../layout/Navbar";
-import Seo from "../../../img/seo.svg";
+import Navbar from '../../layout/Navbar';
+import Seo from '../../../img/seo.svg';
 
 const Apresentacoes = ({
   getApresentacoes,
@@ -18,47 +18,47 @@ const Apresentacoes = ({
   return (
     <>
       <Navbar />
-      <div className="divisao"></div>
-      <section className="bannerTitulo2">
-        <div id="esq">
-          <Link to="/dashboard">
-            <button className="btn-novoManual">
-              <i className="fas fa-arrow-left fa-2x"></i>
+      <div className='divisao'></div>
+      <section className='bannerTitulo2'>
+        <div id='esq'>
+          <Link to='/dashboard'>
+            <button className='btn-novoManual'>
+              <i className='fas fa-arrow-left fa-2x'></i>
             </button>
           </Link>
           <div>
-            <img src={Seo} alt="" />
-            <h1 className="materiaisTit">
+            <img src={Seo} alt='' />
+            <h1 className='materiaisTit'>
               Materiais de <br />
               Marketing
             </h1>
           </div>
         </div>
 
-        <div id="dir">
+        <div id='dir'>
           <div>
-            <Link to="/materiais-graficos">
+            <Link to='/materiais-graficos'>
               <h1>Materiais Gráficos</h1>
             </Link>
             <span>|</span>
-            <h1 className="mg">Apresentações</h1>
+            <h1 className='mg'>Apresentações</h1>
           </div>
 
-          <Link to="/dashboard">
-            <button className="btn-novoManual">
-              <i className="fas fa-plus fa-2x"></i>
+          <Link to='/apresentacoes-upload'>
+            <button className='btn-novoManual'>
+              <i className='fas fa-plus fa-2x'></i>
             </button>
           </Link>
         </div>
       </section>
 
-      <section className="main">
-        <div className="apresentacoesInner">
+      <section className='main'>
+        <div className='apresentacoesInner'>
           {apresentacoes.map(apresentacao => (
             <a href={apresentacao.path} key={apresentacao._id} download>
-              <div className="apresentacao">
+              <div className='apresentacao'>
                 {apresentacao.name}
-                <i className="fas fa-download"></i>
+                <i className='fas fa-download'></i>
               </div>
             </a>
           ))}
